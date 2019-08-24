312c1e9db4065bce2d6b05417d4d205379a427bb
Herb Stahl <ghstahl@gmail.com>
2019-08-24 06:41:51 -0700

    cleanup

--- src/hello-world/hello/world.go
+++
@@ -1,9 +0,0 @@
-package hello
-
-import (
-	"fmt"
-)
-
-var World = func() {
-	fmt.Println("Hello World!")
-}
--- src/hello-world/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"hello-world/hello"
-)
-
-func main() {
-	hello.World()
-}
